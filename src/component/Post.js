76d63e514cf2f7f465c1a441eb394cd856ec97dc
shubhamsdev <shubhams.dev@gmail.com>
2022-04-10 22:17:16 +0530

    insta clone files

---
+++ src/component/Post.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Post = () => {
+    return <div class='post'>
+                <div class='post-top'>
+                    <div class='post-top-left'>
+                        <div class='profile-pic'>
+                            <div class='img-box'>
+                                <img src='' alt='profile pic'/>
+                            </div>
+                        </div>
+                        <div class='profile-name'>
+                            <p class='name'>sam_221b</p>
+                            <p class='time-stamp'>5 min</p>
+                        </div>
+                    </div>
+                    <div class='post-top-right'></div>
+                </div>
+                <div class='post-middle'></div>
+                <div class='post-bottom'></div>
+            </div>
+    
+}
+
+export default Post;
